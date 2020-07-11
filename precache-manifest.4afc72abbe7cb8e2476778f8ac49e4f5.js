73fb76bb6e035f9db38c0e477ff4a2bdee2989ce
Sammie13 <alwaysextra187@gmail.com>
2020-07-11 19:26:11 -0400

    Updates

---
+++ precache-manifest.4afc72abbe7cb8e2476778f8ac49e4f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd314cc1d812eebe7c92cc791b99015",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bea9e31e7a52860c5e5f",
+    "url": "/monsters-rolodex/static/css/main.479c8fea.chunk.css"
+  },
+  {
+    "revision": "5ba5ac096b0d3445fa8f",
+    "url": "/monsters-rolodex/static/js/2.a8bb8d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a8bb8d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea9e31e7a52860c5e5f",
+    "url": "/monsters-rolodex/static/js/main.193a764c.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
